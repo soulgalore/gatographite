ca616351a8625c303a9cbdef5414ab78b3ec4a1b
samattridge <sam.attridge@capita.co.uk>
2016-11-16 15:32:37 +0000

    Update env.sh

--- docker/scripts/env.sh
+++ docker/scripts/env.sh
@@ -4,9 +4,5 @@
 
 # And the full path to the PEM file
 export GA_PEM_PATH=/home/root/my.pem
-# Want to export data based per minute?
-export GA_MINUTES=true
-# Maximum number of results from GA
-export GA_MAX_RESULTS=1000
 # The start of the key namespace sent to Graphite
 export GRAPHITE_NAMESPACE=google.analytics
